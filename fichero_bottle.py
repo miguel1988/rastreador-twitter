ed7eb3f25a0037c36b88facfef4da5453cbe28f1
miguel1988 <migue306@hotmail.com>
2013-05-03 21:18:03 +0300

    Update fichero_bottle.py
    Código que nos permite realizar una petición dinámica a una API
    mostrándonosla en un navegador

--- fichero_bottle.py
+++ fichero_bottle.py
@@ -2,19 +2,20 @@ import bottle
 import requests
 import json
 
-busqueda = requests.get("http://search.twitter.com/search.json", params = {"q":"programar", "lang":"es", "result_type":"recent", "rpp":"1"})
-palabra_buscada= json.loads(busqueda.text)['query']
-comentario = json.loads(busqueda.text)['results'][0]['text']
-imagen_usuario = json.loads(busqueda.text)['results'][0]['profile_image_url']
-delusuario = json.loads(busqueda.text)['results'][0]['from_user']
-creado_en = json.loads(busqueda.text)['results'][0]['created_at']
+@bottle.route('/')
+def home_page():
+    return bottle.template('index')
 
-
-
-@bottle.route('/resultado')
+@bottle.route('/resultado', method='POST')
 def pagina_rasultante():
-    return bottle.template("resultado", rastreando=palabra_buscada, texto=comentario, autor=delusuario, imagen=imagen_usuario, fecha=creado_en)
+    palabrabuscada = bottle.request.forms.get("palabrabuscada")
+	busqueda = requests.get("http://search.twitter.com/search.json", params={"q":palabrabuscada, "lang":"es", "result_type":"recent", "rpp":"1"})
+	palabra_buscada= json.loads(busqueda.text)['query']
+	comentario = json.loads(busqueda.text)['results'][0]['text']
+	imagen_usuario = json.loads(busqueda.text)['results'][0]['profile_image_url']
+	delusuario = json.loads(busqueda.text)['results'][0]['from_user']
+	creado_en = json.loads(busqueda.text)['results'][0]['created_at']
+	return bottle.template('resultado', rastreando=palabra_buscada, texto=comentario, autor=delusuario, imagen=imagen_usuario, fecha=creado_en)
 
-
-
+bottle.debug(True)
 bottle.run(host="localhost", port=8080)
